d4284906ffdb0c87dbf207289c52cac5be5fb2dc
miguel3200 <55986443+miguel3200@users.noreply.github.com>
2020-04-09 15:09:07 -0500

    Add files via upload

---
+++ tabla.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using std::cin;
+using std::cout;
+
+int main() {
+
+	cout << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << "Ascii Table" << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << "\n";
+	for (int i = 33; i < 128; i++) {
+
+		cout << (char)186 << "\t" << i << ":" << (char)i << "\t" << (char)186;
+		cout << (char)186 << "\t" << (i + 128) << ":" << (char)(i + 128) << "\t" << (char)186 << "\n";
+
+
+
+	}
+	cout << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205;
+	return 0;
+}
